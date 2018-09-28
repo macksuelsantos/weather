610f792485093f9f5b803c79a6c05f492a33f63e
Macksuel Santos <macksuell.santos@gmail.com>
2018-09-28 16:14:24 -0300

    - moved MainActivity to package activity;

--- app/src/main/java/com/weather/application/activity/MainActivity.java
+++ app/src/main/java/com/weather/application/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.weather;
+package com.weather.application.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.weather.R;
 
 public class MainActivity extends AppCompatActivity {
 
